7583cd80e30b1e5b0afd709aace7242fe741d4b9
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2025-01-23 09:54:53 +0530

    done

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const UserModel = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
